60fd80a574852fb748dce1687d918b17d836bf44
Shahnawaz Ahmed <shahnawaz.ahmed95@gmail.com>
2017-02-15 02:45:15 +0530

    Added mask

--- script/script.py
+++ script/script.py
@@ -28,7 +28,7 @@ def nmse(data, prediction):
     """
     return (((data - prediction)**2).mean())/sum(data**2)
 
-def leave_one_cross(model, data_slice, gtab):
+def leave_one_cross(model, data_slice, gtab, mask=None):
     """
     Performs a leave one out cross validation on the data points specified by
     `data_slice` and returns the prediction and normalised mean sq error for these
@@ -65,7 +65,7 @@ def leave_one_cross(model, data_slice, gtab):
         # We fit to the data leaving out the current measurement
         left_out_data = np.concatenate([data_slice[..., :left_out], 
                                         data_slice[..., left_out+1:]], -1)
-        fit = current_model.fit(left_out_data)
+        fit = current_model.fit(left_out_data, mask)
         # We try to predict only the left out measurement
         predict_gtab = dpg.gradient_table(np.array([gtab.bvals[left_out]]), 
                                           np.array([gtab.bvecs[left_out]]))
@@ -79,11 +79,13 @@ data = img.get_data()
 x1, y1, z1 = 90, 90, 30
 x2, y2, z2 = 95, 95, 35
 
+mask = data[..., 0] > 100.
+
 predicted_ivim = np.zeros_like(data)
-predicted_ivim[x1:x2, y1:y2, z1:z2, ...] = leave_one_cross(IvimModel, data[x1:x2, y1:y2, z1:z2, :], gtab)
+predicted_ivim = leave_one_cross(IvimModel, data, gtab, mask)
 
 predicted_exp = np.zeros_like(data)
-predicted_exp[x1:x2, y1:y2, z1:z2, ...] = leave_one_cross(ExponentialModel, data[x1:x2, y1:y2, z1:z2, :], gtab)
+predicted_exp = leave_one_cross(ExponentialModel, data, gtab, mask)
 
 exp_img = nib.Nifti1Image(predicted_exp, np.eye(4))
 ivim_img = nib.Nifti1Image(predicted_ivim, np.eye(4))
